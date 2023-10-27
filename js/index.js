a15ffa00c19427d91cb3a12ce0b33a52739b308f
estebankaraputny <estebankaraputny18@gmail.com>
2023-10-27 12:12:11 -0300

    :sparkles: Add: render cards in the section products

--- js/index.js
+++ js/index.js
@@ -1,83 +1,83 @@
 const productsBox = [
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Cajita de regalo",
         descriptionBox: "",
         clientBox: "Macro",
         category: "Alta grafica",
         industria:"Financiera",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/b0adde31c4d1b033db1401c2b21c38b2.png",
         idBox: 1
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja contenedor de autopartes",
         descriptionBox: "",
         clientBox: "Toyota",
         category: "Grandes contenedores",
         industria:"Automotriz",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/c59387f044365b96558c45c1e4ae39e9.png",
         idBox: 2
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja contenedor de liquidos industriales",
         descriptionBox: "",
         clientBox: "Mirgor",
         category: "Envases especiales",
         industria:"Electrónica",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/1496e1036bf05faa1c3c313d008785ed.png",
         idBox: 3
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja contenedor de residuos industriales",
         descriptionBox: "",
         clientBox: "Mirgor",
         category: "Envases especiales",
         industria:"Electrónica",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/2e47487961d23b7c820d553c5018f9d4.png",
         idBox: 4
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja Smart Tv 50'",
         descriptionBox: "",
         clientBox: "Samsung",
         category: "Envases industriales",
         industria:"Electrónica",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/4a5ebe27772bcb14f7944ed0b5f1e1b5.png",
         idBox: 5
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja router Directv",
         descriptionBox: "",
         clientBox: "Directv",
         category: "Envases industriales",
         industria:"Telecomunicaciones",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/9bcb61dea1586a476b3777f57dd9191e.png",
         idBox: 6
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Cajita de regalo",
         descriptionBox: "",
         clientBox: "Disco",
         category: "Alta grafica",
         industria:"Hipermercado",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/20e1819cf7d9272e4622d44dd0512ffc.png",
         idBox: 7
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja Smart Tv 43'",
         descriptionBox: "",
         clientBox: "LG",
         category: "Envases industriales",
         industria:"Electrónica",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/a9d209dbcf9c3586ede0f894a38343c4.png",
         idBox: 8
     },
     {
-        titleBox: "Caja de cartón",
+        titleBox: "Caja autopartes",
         descriptionBox: "",
         clientBox: "Iveco",
         category: "Envases industriales",
         industria:"Automotriz",
-        imgUrl: "",
+        imgUrl: "https://www.argenpack.com/files/7c6ef7d4c1bf81fb27d4607acf0b8974.png",
         idBox: 9
     },
 ]
@@ -118,3 +118,39 @@ window.onload = function(){
     fadeOut(loaderPage);
     bodyPage.classList.remove("hiddenBody");
 };
+
+
+// RENDER CARD PRODCUTS 
+
+const contentCards = document.querySelector("#contentProducts");
+    let card = `
+    <div class="content__messageErr" id="messageError">
+    </div>
+    `;
+
+const renderCards = () => {
+    productsBox.map((product) => {
+        return ( card += `
+        <div class="card-pro">
+          <div class="cont-img-card">
+            <img src="${product.imgUrl}" alt="Imagen del producto seleccionado y personalizado por el cliente." class="img-card">
+          </div>
+          <div class="cont-info-card">
+            <h5 class="title-card">${product.titleBox}</h5>
+            <p class="price-card">Cliente: ${product.clientBox}</p>
+            </div>
+            <div class="cont-button-card">
+              <a href="#" class="button-card">Ver más</a>
+            </div>
+        </div>
+        `
+        );
+    })
+    contentCards.innerHTML = card;
+}
+
+renderCards();
+
+
+
+
