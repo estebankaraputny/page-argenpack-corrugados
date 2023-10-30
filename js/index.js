9a42cff2fe5136c0ee613f618c299ad8a3fe1197
estebankaraputny <estebankaraputny18@gmail.com>
2023-10-30 18:47:31 -0300

    :bug: Fix: fix in everybody page for responsive sitie

--- js/index.js
+++ js/index.js
@@ -133,38 +133,38 @@ if(screen.width < 850){
 
 
 
+if( window.location.pathname === "/pages/product.html"){
 
-
-
+    
 
 // RENDER CARD PRODCUTS 
 
 const contentCards = document.querySelector("#contentProducts");
-    let card = `
-    <div class="content__messageErr" id="messageError">
-    </div>
-    `;
+let card = `
+<div class="content__messageErr" id="messageError">
+</div>
+`;
 
 const renderCards = () => {
-    productsBox.map((product) => {
-        return ( card += `
-        <div class="card-pro" id="cardProduct">
-          <div class="cont-img-card">
-            <img src="${product.imgUrl}" alt="Imagen del producto seleccionado y personalizado por el cliente." class="img-card">
-          </div>
-          <div class="cont-info-card">
-            <h5 class="title-card">${product.titleBox}</h5>
-            <p class="price-card">Cliente: ${product.clientBox}</p>
-            </div>
-            <div class="cont-button-card">
-              <a href="./details.html" class="button-card" id="buttonDetails">Ver más</a>
-            </div>
-            <span class="idOfBox" id="${product.idBox}"></span>
+productsBox.map((product) => {
+    return ( card += `
+    <div class="card-pro" id="cardProduct">
+      <div class="cont-img-card">
+        <img src="${product.imgUrl}" alt="Imagen del producto seleccionado y personalizado por el cliente." class="img-card">
+      </div>
+      <div class="cont-info-card">
+        <h5 class="title-card">${product.titleBox}</h5>
+        <p class="price-card">Cliente: ${product.clientBox}</p>
         </div>
-        `
-        );
-    })
-    contentCards.innerHTML = card;
+        <div class="cont-button-card">
+          <a href="./details.html" class="button-card" id="buttonDetails">Ver más</a>
+        </div>
+        <span class="idOfBox" id="${product.idBox}"></span>
+    </div>
+    `
+    );
+})
+contentCards.innerHTML = card;
 }
 
 renderCards();
@@ -178,52 +178,50 @@ const cardBoxs = document.querySelectorAll("#cardProduct");
 
 
 inputSearch.addEventListener("keyup", (event) =>{
-    
-    console.log(event.target.value.toLowerCase())
 
-    let hiddenCard = [];
+console.log(event.target.value.toLowerCase())
 
-    // console.log(cardBoxs.forEach(box => {
-    //     console.log(box)
-    // }));
-    
-    if(event.target.value != " "){
-        cardBoxs.forEach( box =>{
-            
-            box.textContent.toLocaleLowerCase().replace(" ", "-").includes(event.target.value.toLocaleLowerCase().replace(" ", "-"))
-            ? box.classList.remove("hidden")
-            : box.classList.add("hidden")
+let hiddenCard = [];
 
-            if(box.classList.contains("hidden")){
-                hiddenCard.push(box);
-            } 
-        })
-    };
+// console.log(cardBoxs.forEach(box => {
+//     console.log(box)
+// }));
 
-    const messageError = document.getElementById("messageError");
-    let message = ``;
+if(event.target.value != " "){
+    cardBoxs.forEach( box =>{
+        
+        box.textContent.toLocaleLowerCase().replace(" ", "-").includes(event.target.value.toLocaleLowerCase().replace(" ", "-"))
+        ? box.classList.remove("hidden")
+        : box.classList.add("hidden")
 
-    if(hiddenCard.length === cardBoxs.length){
-        message += `
-        <div class="card__mensajeDeError">
-            <img src="../assent/image/caja-error.jpg" alt="Imagen del espacio" class="img__error">
-            <div class="mensaje__error">
-                <h2>No hay productos que coincidan con tu búsqueda.</h2>
-                <ul>
-                    <li>Revisá la ortografía de la palabra.</li>
-                    <li>Utilizá palabras más genéricas o menos palabras.</li>
-                    <li>Prueba navegar por nuestro menú de categorías.</li>
-                    <li>Vuelve a la página principal para seguir buscando.</li>
-                </ul>
-            </div>
-        </div> `
-    }
+        if(box.classList.contains("hidden")){
+            hiddenCard.push(box);
+        } 
+    })
+};
 
-    messageError.innerHTML = message;
+const messageError = document.getElementById("messageError");
+let message = ``;
+
+if(hiddenCard.length === cardBoxs.length){
+    message += `
+    <div class="card__mensajeDeError">
+        <img src="../assent/image/caja-error.jpg" alt="Imagen del espacio" class="img__error">
+        <div class="mensaje__error">
+            <h2>No hay productos que coincidan con tu búsqueda.</h2>
+            <ul>
+                <li>Revisá la ortografía de la palabra.</li>
+                <li>Utilizá palabras más genéricas o menos palabras.</li>
+                <li>Prueba navegar por nuestro menú de categorías.</li>
+                <li>Vuelve a la página principal para seguir buscando.</li>
+            </ul>
+        </div>
+    </div> `
+}
+
+messageError.innerHTML = message;
 
 }); 
-
-
 
 
 // ENVIAR IDBOX PARA RENDERIZAR 
@@ -232,9 +230,79 @@ const buttonVerMas = document.querySelectorAll("#buttonDetails");
 let idOfBox = document.querySelectorAll(".idOfBox");
 
 for(let i = 0; i < idOfBox.length; i++){
-    buttonVerMas[i].addEventListener( "click", () => {
-        console.log(idOfBox[i].id);
-        localStorage.setItem("idOfBox", idOfBox[i].id);
-    });
+buttonVerMas[i].addEventListener( "click", () => {
+    console.log(idOfBox[i].id);
+    localStorage.setItem("idOfBox", idOfBox[i].id);
+});
 }
 
+} else if (window.location.pathname === "/pages/details.html"){
+    
+
+let idBoxProduct = localStorage.getItem("idOfBox");
+
+console.log(idBoxProduct);
+
+const renderDetails = () => {
+    let main = "";
+    
+    let contentDetails = document.getElementById("detailsBox");
+
+    for(let i = 0; i < productsBox.length; i++){
+        if(productsBox[i].idBox == idBoxProduct){
+            main += `
+            <div>
+                <a href="./product.html" class="button-volver"><i class="bi bi-arrow-left-circle"></i></a>
+            </div>
+            <div class="card-details">
+                <div class="cont-img-detials" id="img-container">
+                    <img class="img-details" src="${productsBox[i].imgUrl}" alt="Imagen del producto">
+                </div>
+                <div class="cont-info-details">
+                    <span class="ruta-details"><a href="../index.html">Inicio</a> / <a href="./product.html">Productos</a> / ${productsBox[i].titleBox}</span>
+                    <div class="cont-more-info-details">
+                        <h3 class="title-details">${productsBox[i].titleBox}</h3>
+                        <p class="client-details">Cliente ${productsBox[i].clientBox}</p>
+                        <div class="cont-description-details">
+                            <p class="subtitle-details">Descripción</p>
+                            <p class="info-details">
+                                ${productsBox[i].descriptionBox}
+                            </p>
+                            <p class="danger-details">
+                                <i class="bi bi-exclamation-circle"></i>  Para obtener tu producto personalizado o saber más sobre nuestra propuestas, por favor contactanos a traves de nuestro formulario de contacto.
+                            </p>
+                        </div>
+                        <div class="cont-button-details">
+                            <a class="submit-contact button-details" href="./contact.html">Contactar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        }
+        contentDetails.innerHTML = main;
+    }
+}
+renderDetails();
+
+
+// // ZOOM IMG 
+
+// let options = {
+
+//     fiilContainer:true,
+//     // width: 300,
+//     // heigth: 300,
+//     zoomWidth: 200,
+//     offSet: {
+//         vertical: 20,
+//         horizontal: 10
+//     },
+//     // zoomStyle: 'background-color: white;',
+//     // zoomPosition (string) - position of zoomed image. It can be: top, left, bottom, original or the default right.
+//     // zoomPosition:"top: 200; rigth: 500;",
+//     scale: 3
+// }
+
+// new ImageZoom(document.getElementById("img-container"), options);
+}
